616d1fbfde5fca902bb76c14a0265cdec5fa7f2b
mibzman <sam@hungerperks.com>
2019-04-24 08:42:26 -0400

    added prototype RSS

--- API.go
+++ API.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
+	"github.com/gorilla/feeds"
 	"github.com/gorilla/mux"
 )
 
@@ -15,9 +17,10 @@ type APIManager struct {
 
 func (APIManager APIManager) AddRoutes() {
 	APIManager.Router.HandleFunc("/reddit", APIManager.RedditHandler)
+	APIManager.Router.HandleFunc("/redditRSS", APIManager.RedditRSSHandler)
 }
 
-func (APIManager APIManager) RedditHandler(w http.ResponseWriter, r *http.Request) {
+func (APIManager APIManager) GetTodaysPosts() []Post {
 	Choice := "Today"
 	Digests := APIManager.config.getDigests(Choice)
 
@@ -26,6 +29,12 @@ func (APIManager APIManager) RedditHandler(w http.ResponseWriter, r *http.Reques
 		Digest.populatePosts(APIManager.redditBot, Choice)
 		Posts = append(Posts, Digest.Posts.toArray()...)
 	}
+	return Posts
+}
+
+func (APIManager APIManager) RedditHandler(w http.ResponseWriter, r *http.Request) {
+
+	Posts := APIManager.GetTodaysPosts()
 
 	encoder := json.NewEncoder(w)
 	err := encoder.Encode(Posts)
@@ -36,12 +45,49 @@ func (APIManager APIManager) RedditHandler(w http.ResponseWriter, r *http.Reques
 	}
 }
 
-func WriteJSONResponse(w http.ResponseWriter, Data ...interface{}) {
-	encoder := json.NewEncoder(w)
-	err := encoder.Encode(Data)
+func (APIManager APIManager) RedditRSSHandler(w http.ResponseWriter, r *http.Request) {
+
+	Posts := APIManager.GetTodaysPosts()
+	now := time.Now()
+
+	feed := feeds.Feed{
+		Title:       "Reddit RSS",
+		Description: "all my reddit stuff",
+		Author:      &feeds.Author{Name: "sam borick"},
+		Created:     now,
+	}
+
+	Items := []*feeds.Item{}
+
+	for _, Post := range Posts {
+		Item := &feeds.Item{
+			Title:       Post.Title,
+			Link:        &feeds.Link{Href: Post.URL},
+			Description: Post.SelfTextHTML,
+			Created:     now,
+		}
+		Items = append(Items, Item)
+	}
+
+	feed.Items = Items
+
+	rss, err := feed.ToRss()
 	if err != nil {
-		w.Write([]byte("Failed to encode json:" + err.Error()))
+		w.Write([]byte("Failed to encode rss:" + err.Error()))
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	w.Write([]byte(rss))
+
 }
+
+// func WriteJSONResponse(w http.ResponseWriter, Data ...interface{}) {
+// 	encoder := json.NewEncoder(w)
+// 	err := encoder.Encode(Data)
+// 	if err != nil {
+// 		w.Write([]byte("Failed to encode json:" + err.Error()))
+// 		w.WriteHeader(http.StatusInternalServerError)
+// 		return
+// 	}
+// }
